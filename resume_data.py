e3da589b4f93337797b8ce27756f40064c8ef9ac
Kent <kkawahara1028@gmail.com>
2015-02-21 14:26:30 -0800

    added static files and started work on UserData

--- resume_data.py
+++ resume_data.py
@@ -1,17 +1,22 @@
-# NOTE 'a python object' needs to be a standardized thing
+#For handling the user data in a clean-ish and standardized way
 
-# Takes a python object and returns JSON for a cookie
-def serialize(data):
-	pass
-
-# Takes a JSON object and returns a python object
-def deserialize(data):
-	pass
-
-# Takes a python dictionary representation of our form and returns a python object
-def parse_http_form(formList):
-	pass
-
-# Takes a python object and returns a python file descriptor to /static/tmp/<session_id>_<secure_filename>.pdf containing the pdf resume
-def generate_pdf_resume(data):
-	pass
+class UserData:
+    def __init__(self):
+        self.personal_info = []
+    
+    # Takes a python object and returns an object for a cookie
+    def serialize(data):
+        s = ""
+        for group in data:
+    
+    # Takes an object and returns a python object
+    def deserialize(data):
+        pass
+    
+    # Takes a python dictionary representation of our form and returns a python object
+    def parse_http_form(formList):
+        pass
+    
+    # Takes a python object and returns a python file descriptor to /static/tmp/<session_id>_<secure_filename>.pdf containing the pdf resume
+    def generate_pdf_resume(data):
+        pass
