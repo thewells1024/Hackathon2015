bda1e6268ceb2896aec464a9623f388036f996bd
Kent <kkawahara1028@gmail.com>
2015-02-21 15:12:32 -0800

    fixed tab issue

--- resume_data.py
+++ resume_data.py
@@ -1,45 +1,45 @@
 # For handling the user data in a clean-ish and standardized way
 
 """
-personal_info 		dict(str:str) 					= {"name": "John Smith", "phone": "(123) 456-7890", "location": "Canada", "email": "jsmith@calpoly.edu"}
+personal_info         dict(str:str)                     = {"name": "John Smith", "phone": "(123) 456-7890", "location": "Canada", "email": "jsmith@calpoly.edu"}
 
-education 			list(dict(str:str)) 			= [ {"school_name": "Cuesta", "degree": "BS Things", "status": "in progress", "year": "2018"} ]
+education             list(dict(str:str))             = [ {"school_name": "Cuesta", "degree": "BS Things", "status": "in progress", "year": "2018"} ]
 
-work_experience 	list(dict(str:str)) 			= [ {"employer": "Stringy.com", "location": "Anytown, USA", "time period": "2013-2014", "comments": ["str1", "str2", "str3"]}]
+work_experience     list(dict(str:str))             = [ {"employer": "Stringy.com", "location": "Anytown, USA", "time period": "2013-2014", "comments": ["str1", "str2", "str3"]}]
 
-projects 			list(dict(str:str)) 			= [ {"title": "Resume Website", "summary": "Text Block", "comments": ["a thing", "moar thing", "even more thang"]} ]
+projects             list(dict(str:str))             = [ {"title": "Resume Website", "summary": "Text Block", "comments": ["a thing", "moar thing", "even more thang"]} ]
 
-skills 				list(str) 						= ["A string", "Another string", "A third string", "Fourth string quarterback"]
+skills                 list(str)                         = ["A string", "Another string", "A third string", "Fourth string quarterback"]
 
 """
 
 def dict_count(fd, string):
-	return len([x for x in fd.keys() if x.find(string) >= 0])
+    return len([x for x in fd.keys() if x.find(string) >= 0])
 
 def dict_sketchy_shit(fd, string, keys):
-	temp = []
-	for i in range(dict_count(fd, string)):
-		temp.append({})
-		for k in keys:
-			temp[i][k] = fd[string+str(i)+k]
-	return temp
+    temp = []
+    for i in range(dict_count(fd, string)):
+        temp.append({})
+        for k in keys:
+            temp[i][k] = fd[string+str(i)+k]
+    return temp
 
 class UserData:
     def __init__(self, fd):
-		pi = {}
-		pi['name'] = fd['name']
-		pi['phone'] = fd['phone']
-		pi['location'] = fd['location']
-		pi['email'] = fd['email']
+        pi = {}
+        pi['name'] = fd['name']
+        pi['phone'] = fd['phone']
+        pi['location'] = fd['location']
+        pi['email'] = fd['email']
         self.personal_info = pi
 
-		self.education = dict_sketchy_shit(fd, "school", ["name", "degree", "status", "year"])
+        self.education = dict_sketchy_shit(fd, "school", ["name", "degree", "status", "year"])
 
-		self.work_experience = dict_sketchy_shit(fd, "job", ["employer", "location", "time period", "comments"])
+        self.work_experience = dict_sketchy_shit(fd, "job", ["employer", "location", "time period", "comments"])
 
-		self.projects = dict_sketchy_shit(fd, "project", ["title", "summary", "comments"])
-		
-		self.skills = [skill for skill in fd['skills'].split("\n")]
+        self.projects = dict_sketchy_shit(fd, "project", ["title", "summary", "comments"])
+        
+        self.skills = [skill for skill in fd['skills'].split("\n")]
     
     # Takes a python object and returns an object for a cookie
     def serialize(data):
--- site.py
+++ site.py
@@ -6,43 +6,43 @@ app = Flask(__name__)
 # Homepage
 @app.route('/')
 def homepage():
-	return render_template('homepage.html')
+    return render_template('homepage.html')
 
 # Start of the resume-generation process
 @app.route('/form/', methods = ['GET', 'POST'])
 def resume_info_form():
-	if request.method == 'GET':
-		return render_template('form.html')
-	else:
-		data = UserData(request.form)
-		response = make_response(redirect(url_for('resume')))
-		response.set_cookie(serialize(data))
-		return response
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        data = UserData(request.form)
+        response = make_response(redirect(url_for('resume')))
+        response.set_cookie(serialize(data))
+        return response
 
 @app.route('/resume/')
 def resume_generator():
-	data = deserialize(request.cookies.get('form_data'))
-	pdf = generate_pdf_resume(data)
-	return render_template('resume.html', pdf=pdf)
+    data = deserialize(request.cookies.get('form_data'))
+    pdf = generate_pdf_resume(data)
+    return render_template('resume.html', pdf=pdf)
 
 # Page about making resumes
 @app.route('/info/')
 def resume_info():
-	return render_template('resume_info.html')
+    return render_template('resume_info.html')
 
 @app.route('/about/')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
 
 # Mockup page for testing
 @app.route('/mockup/')
 def mockup():
-	return render_template('mockup.html')
+    return render_template('mockup.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
