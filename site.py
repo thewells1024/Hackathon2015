b26746b6e1063094ec70476b10ca8ca7fa98e57b
deltaheavy <mzinkus@calpoly.edu>
2015-02-21 12:36:01 -0800

    Add template, more code

--- site.py
+++ site.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# Homepage
 @app.route('/')
 def homepage():
 	return render_template('homepage.html')
 
+# Start of the resume-generation process
 @app.route('/form/', methods = ['GET', 'POST'])
 def resume_info_form():
 	if request.method == 'GET':
@@ -13,11 +15,10 @@ def resume_info_form():
 		# get their info from the POST
 		return # something
 
+# Page about making resumes
 @app.route('/info/')
-def resume_ideas():
-	# static page about making resumes
-	return
-
+def resume_info():
+	return render_template('resume_info.html')
 
 if __name__ == '__main__':
 	app.debug = True
