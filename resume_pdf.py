dfa4d1759fccc361a8f569d471c84ca0af8d3095
deltaheavy <mzinkus@calpoly.edu>
2015-02-21 19:13:39 -0800

    Continue work on pdf generation

--- resume_pdf.py
+++ resume_pdf.py
@@ -1,5 +1,6 @@
 from time import time
 from random import random
+import subprocess
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
@@ -10,17 +11,19 @@ from reportlab.lib.units import inch
 def get_secure_filename():
     s = str(time()).replace('.', '')
     s+= str(random())[2:]
-    return '/static/resumes/' + s + '.pdf'
+    return 'static/resumes/' + s + '.pdf'
 
 def generate_pdf_from_data(data):
     filename = get_secure_filename()
+    subprocess.check_call(['touch', filename])
+        
     doc = SimpleDocTemplate(filename,pagesize=letter, rightMargin=72,leftMargin=72,topMargin=72,bottomMargin=18)
-    Story=[]
+    Story = []
     
     styles=getSampleStyleSheet()
     styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
-    styles.add(ParagraphStyle(name='Left', alignment=TA_CENTER))
-    styles.add(ParagraphStyle(name='Right', alignment=TA_CENTER))
+    styles.add(ParagraphStyle(name='Left', alignment=TA_LEFT))
+    styles.add(ParagraphStyle(name='Right', alignment=TA_RIGHT))
     
     Story.append(Paragraph('<font size=18>%s</font>' % data.personal_info['name'], styles["Center"]))
     Story.append(Spacer(1, 12))
@@ -32,7 +35,7 @@ def generate_pdf_from_data(data):
     Story.append(Paragraph('<font size=12 style=bold>Education</font>', styles["Left"]))    
     Story.append(Paragraph('<font size=12>%s</font>' % data.education[0]['name'], styles["Left"]))
     
-    Story.append(Paragraph('<font size=12 style=bold>Experience</font>', styles["Left"]))
+    Story.append(Paragraph('<font size=12 bold>Experience</font>', styles["Left"]))
 
     Story.append(Paragraph('<font size=12 style=bold>Projects</font>', styles["Left"]))
 
@@ -41,4 +44,4 @@ def generate_pdf_from_data(data):
         Story.append(Paragraph('<font size=12>%s</font>' % skill, styles['Left']))
 
     doc.build(Story)
-    return filename 
+    return '/' + filename 
